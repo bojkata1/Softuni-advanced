a7e8fb3970b587bbe224c55b7e362194551229c6
bojkata1 <bojidar.enchev03@abv.bg>
2023-09-25 22:38:36 +0300

    More exercises

--- Multidimensional list - Exercise 1/ex1.py
+++ Multidimensional list - Exercise 1/ex1.py
@@ -1,19 +1,7 @@
 n = int(input())
-matrix = []
-for i in range(n):
-    elements = [x for x in input().split(', ')]
-    matrix.append(elements)
-prime_diagonal = []
-prime_sum = 0
-secondary_diagonal = []
-secondary_sum = 0
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            prime_diagonal += matrix[i][j]
-            prime_sum += int(matrix[i][j])
-        if (i + j) == (n - 1):
-            secondary_diagonal += matrix[i][j]
-            secondary_sum += int(matrix[i][j])
-print(f"Primary diagonal: {', '.join(prime_diagonal)}. Sum: {prime_sum}")
-print(f"Secondary diagonal: {', '.join(secondary_diagonal)}. Sum: {secondary_sum}")
+matrix = [[int(x) for x in input().split(', ')] for _ in range(n)]
+prime_diagonal = [matrix[i][i] for i in range(n)]
+secondary_diagonal = [matrix[i][-i - 1] for i in range(n)]
+
+print(f"Primary diagonal: {', '.join([str(x) for x in prime_diagonal])}. Sum: {sum(prime_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
